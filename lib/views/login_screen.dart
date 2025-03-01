6246aaeb4f83c9c7ad10883c8d53ce073647e198
DongKey03 <dongpham24703@gmail.com>
2025-03-01 18:09:57 +0700

    update login_screen

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -15,14 +15,20 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Padding(
         padding: EdgeInsets.all(25),
         child: SafeArea(
-          child: Column(
-            children: [
-              Title(),
-              SizedBox(height: 20),
-              welcomeText(),
-              SizedBox(height: 40),
-              textForm(),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Title(),
+                SizedBox(height: 20),
+                welcomeText(),
+                SizedBox(height: 40),
+                TextFormFieldWithIcon(),
+                SizedBox(height: 5),
+                btnForgetPassword(),
+                SizedBox(height: 30),
+                btnLogin(),
+              ],
+            ),
           ),
         ),
       ),
@@ -88,24 +94,193 @@ class welcomeText extends StatelessWidget {
   }
 }
 
-class textForm extends StatelessWidget {
-  const textForm({super.key});
+class TextFormFieldWithIcon extends StatefulWidget {
+  const TextFormFieldWithIcon({super.key});
+
+  @override
+  State<TextFormFieldWithIcon> createState() => _TextFormFieldWithIconState();
+}
+
+class _TextFormFieldWithIconState extends State<TextFormFieldWithIcon> {
+  bool _isPasswordVisible = false; // Biến kiểm tra mật khẩu có hiển thị hay không
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.start,
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('Email or Mobile Number'),
+        const Text(
+          'Email or Mobile Number',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
         TextField(
           keyboardType: TextInputType.emailAddress,
           decoration: InputDecoration(
-            hintText: 'Enter your emmail or mobile number',
-            border: OutlineInputBorder(),
+            fillColor: const Color(0xffECF1FF),
+            // mau nen cua textfield
+            filled: true,
+            // bat che do dien mau nen cho textfield
+            hintText: 'Enter your email or mobile number',
+            hintStyle: const TextStyle(
+              color: Color(0xff809CFF),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
           ),
         ),
-        Text('Password'),
+        const SizedBox(height: 15),
+        const Text(
+          'Password',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          obscureText: !_isPasswordVisible, // Kiểm tra trạng thái hiển thị mật khẩu
+          decoration: InputDecoration(
+            fillColor: const Color(0xffECF1FF),
+            filled: true,
+            hintText: 'Enter your password',
+            hintStyle: const TextStyle(
+              color: Color(0xff809CFF),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            suffixIcon: IconButton(
+              icon: Icon(
+                _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                color: Colors.black, // Bạn có thể thay đổi màu sắc của icon nếu cần
+              ),
+              onPressed: () {
+                setState(() {
+                  _isPasswordVisible = !_isPasswordVisible; // Đổi trạng thái khi nhấn vào biểu tượng
+                });
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class btnForgetPassword extends StatelessWidget {
+  const btnForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: const Text(
+            'Forget Password',
+            style: TextStyle(
+              color: Color(0xff2260FF),
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class btnLogin extends StatelessWidget {
+  const btnLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xff2260FF),
+            minimumSize: const Size(250, 50), // Đặt chiều rộng và chiều cao của button
+          ),
+          child: const Text(
+            'Log In',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+        const Text('Or sign up with'),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              style: IconButton.styleFrom(
+                backgroundColor: const Color(0xff2260FF),
+              ),
+              icon: Image.asset(
+                'assets/images/login_screen/Ellipse 35.png',
+                width: 24,
+                height: 24,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              style: IconButton.styleFrom(
+                backgroundColor: const Color(0xff2260FF),
+              ),
+              icon: Image.asset(
+                'assets/images/login_screen/fb.png',
+                width: 24,
+                height: 24,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              style: IconButton.styleFrom(
+                backgroundColor: const Color(0xff2260FF),
+              ),
+              icon: Image.asset(
+                'assets/images/login_screen/Vector 164.png',
+                width: 24,
+                height: 24,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 30),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Don't have  an account?"),
+            GestureDetector(
+              onTap: () {},
+              child: const Text(
+                ' Sign Up',
+                style: TextStyle(
+                  color: Color(0xff2260FF),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        )
       ],
     );
   }
